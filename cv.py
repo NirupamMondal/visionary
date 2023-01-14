83615ed70fc80aceebcee34c24d514a87ed3c9d8
NirupamMondal <73392723+NirupamMondal@users.noreply.github.com>
2023-01-14 13:11:14 +0530

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as num
+import face_recognition
+
+imgniru=face_recognition.load_image_file("open cv/bluetooth/niru.jpg")
+imgniru=cv2.cvtColor(imgniru.cv2.COLOR_BGR2RGB)
+
+
